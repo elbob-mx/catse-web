bf47b410717e853f82942f09ff56ce45a6acd2e6
Roberto Gutierrez <rob7th@gmail.com>
2026-01-15 00:18:41 -0600

    vite config fix for netlify

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [tailwindcss()],
+    build: {
+        rollupOptions: {
+            input: {
+                // Aquí le decimos a Vite todas las páginas que deben existir
+                main: "./index.html",
+                conocenos: "./conocenos.html",
+                // Cuando crees 'genios.html', lo añadirás aquí abajo
+            },
+        },
+    },
 });
