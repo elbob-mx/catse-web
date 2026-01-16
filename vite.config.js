0ab6dbf46d401659d057da8dafde370a6d2c1969
Roberto Gutierrez <rob7th@gmail.com>
2026-01-16 17:53:30 -0600

    vite fixed

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    base: "./",
     build: {
         rollupOptions: {
             input: {
@@ -11,6 +12,12 @@ export default defineConfig({
                 genios: resolve(__dirname, "genios.html"),
                 blog: resolve(__dirname, "blog.html"),
                 contacto: resolve(__dirname, "contacto.html"),
+            },
+            output: {
+                // Esto asegura que el JS se llame main.js y el CSS style.css
+                entryFileNames: `[name].js`,
+                chunkFileNames: `[name].js`,
+                assetFileNames: `[name].[ext]`,
             },
         },
     },
