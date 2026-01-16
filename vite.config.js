8866dab77be568d4afae43cb618a787e5bf94f10
Roberto Gutierrez <rob7th@gmail.com>
2026-01-16 17:29:25 -0600

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [tailwindcss()],
     build: {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
                 conocenos: resolve(__dirname, "conocenos.html"),
                 servicios: resolve(__dirname, "servicios.html"),
+                genios: resolve(__dirname, "genios.html"),
                 blog: resolve(__dirname, "blog.html"),
                 contacto: resolve(__dirname, "contacto.html"),
             },
